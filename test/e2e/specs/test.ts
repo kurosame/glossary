b86b2ae865f89d08fd2213582b5475ffb05c8514
kurosame <kurosame1227@gmail.com>
2018-07-05 11:39:36 +0900

    bugfix

--- test/e2e/specs/test.ts
+++ test/e2e/specs/test.ts
@@ -21,12 +21,13 @@ afterAll(() => {
   browser.close()
 })
 
-beforeEach(async () => {
-  await page.goto('http://localhost:9000')
-})
+// beforeEach(async () => {
+//   await page.goto('http://localhost:9000')
+// })
 
 test('Initial display', async () => {
-  // await page.waitFor(1000)
+  await page.goto('http://localhost:9000')
+  await page.waitFor(1000)
 
   await page.screenshot({
     path: path.join(__dirname, '__screenshots__', 'init.png'),
