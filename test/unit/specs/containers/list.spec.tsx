81044bcaef9ce4aca8befad99a9fc250ffa1763b
kurosame <kurosame1227@gmail.com>
2018-07-17 18:55:08 +0900

    bugfix: unit test

--- test/unit/specs/containers/list.spec.tsx
+++ test/unit/specs/containers/list.spec.tsx
@@ -3,12 +3,25 @@ import { shallow } from 'enzyme'
 import React from 'react'
 import configureStore from 'redux-mock-store'
 
-const states = { words: [] }
+const states = {
+  words: [{ category: 'firebase', titles: [], descriptions: [] }]
+}
 
-const wrapper = shallow(<List />, {
-  context: { store: configureStore()(states) }
+test('Match the snapshot when category exists', () => {
+  const wrapper = shallow(
+    <List match={{ params: { category: 'firebase' } }} />,
+    {
+      context: { store: configureStore()(states) }
+    }
+  )
+
+  expect(wrapper.html()).toMatchSnapshot()
 })
 
-test('Match the snapshot', () => {
+test('Match the snapshot when category is nothing', () => {
+  const wrapper = shallow(<List match={{ params: { category: '' } }} />, {
+    context: { store: configureStore()(states) }
+  })
+
   expect(wrapper.html()).toMatchSnapshot()
 })
