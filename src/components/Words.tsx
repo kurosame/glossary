d65c050b756088f8c21137087832c23e52d4387e
kurosame <kurosame1227@gmail.com>
2019-04-26 18:30:13 +0900

    fix: category filter from Words to List

--- src/components/Words.tsx
+++ src/components/Words.tsx
@@ -1,36 +1,28 @@
 import SearchBar from '@/components/SearchBar'
 import Word from '@/components/Word'
-import { IStates } from '@/modules/states'
-import { getWords, IWordActions, IWordState } from '@/modules/word'
+import { IWordState } from '@/modules/word'
 import { List, ListItem } from '@material-ui/core'
 import React from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators, Dispatch } from 'redux'
 import styled from 'styled-components'
 
 interface IProps {
-  state: { words: IWordState[] }
-  actions: IWordActions
-  category: string
+  words: IWordState[]
 }
 
 interface IState {
-  filterWords: IWordState[]
+  filterWords: IWordState[] | undefined
 }
 
-export class Words extends React.PureComponent<IProps, IState> {
+class Words extends React.PureComponent<IProps, IState> {
   constructor(props: IProps) {
     super(props)
-    if (!props.state.words.length) {
-      props.actions.getWords()
-    }
     this.state = {
-      filterWords: []
+      filterWords: undefined
     }
   }
 
   public setFilterWords(e: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ filterWords: e.target.value ? [] : this.props.state.words })
+    this.setState({ filterWords: e.target.value ? [] : undefined })
   }
 
   public render() {
@@ -39,11 +31,8 @@ export class Words extends React.PureComponent<IProps, IState> {
         <ListItem>
           <SearchBar onSearch={e => this.setFilterWords(e)} />
         </ListItem>
-        {this.props.state.words
-          .filter(
-            w => !this.props.category || w.category === this.props.category
-          )
-          .map(w => (
+        {this.state.filterWords ||
+          this.props.words.map(w => (
             <ListItem key={w.id}>
               <WordDiv>
                 <Word word={w} />
@@ -58,12 +47,4 @@ export class Words extends React.PureComponent<IProps, IState> {
 const WordDiv = styled.div`
   width: 100%;
 `
-
-export default connect(
-  (states: IStates) => ({ state: { words: states.words } }),
-  (dispatch: Dispatch) => ({
-    actions: {
-      getWords: bindActionCreators(getWords, dispatch)
-    }
-  })
-)(Words)
+export default Words
--- src/containers/List.tsx
+++ src/containers/List.tsx
@@ -1,10 +1,42 @@
 import Words from '@/components/Words'
+import { IStates } from '@/modules/states'
+import { getWords, IWordActions, IWordState } from '@/modules/word'
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators, Dispatch } from 'redux'
 
 interface IProps {
+  state: { words: IWordState[] }
+  actions: IWordActions
   match: { params: { category: string } }
 }
 
-const List = (props: IProps) => <Words category={props.match.params.category} />
+class List extends React.PureComponent<IProps> {
+  constructor(props: IProps) {
+    super(props)
+    if (!props.state.words.length) {
+      props.actions.getWords()
+    }
+  }
 
-export default List
+  public render() {
+    return (
+      <Words
+        words={this.props.state.words.filter(
+          w =>
+            !this.props.match.params.category ||
+            w.category === this.props.match.params.category
+        )}
+      />
+    )
+  }
+}
+
+export default connect(
+  (states: IStates) => ({ state: { words: states.words } }),
+  (dispatch: Dispatch) => ({
+    actions: {
+      getWords: bindActionCreators(getWords, dispatch)
+    }
+  })
+)(List)
