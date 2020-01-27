e06b6b078c091a2cc1e8b534cca573401206038e
kurosame <kurosame1227@gmail.com>
2020-01-27 18:25:01 +0900

    fix: unit test

--- test/unit/setup.ts
+++ test/unit/setup.ts
@@ -1,4 +1,9 @@
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import React from 'react'
 
 configure({ adapter: new Adapter() })
+
+// Avoid the following warning
+// Warning: useLayoutEffect does nothing on the server,...
+React.useLayoutEffect = React.useEffect
