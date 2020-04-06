ec14722e951a360df273de46f2361f493b733629
kurosame <kurosame1227@gmail.com>
2020-04-06 19:44:31 +0900

    fix: no push message in foreground, firebase downgrade

--- src/firebase/messaging-sw.js
+++ src/firebase/messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/6.2.4/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/6.2.4/firebase-messaging.js')
 
 firebase.initializeApp({ messagingSenderId: '167100381499' })
-const messaging = firebase.messaging()
+firebase.messaging()
 
 self.addEventListener('notificationclick', e => {
   e.notification.close()
