acebb62a4c13e50dda3242cbd0307ecf5c6ebb6f
kurosame <kurosame1227@gmail.com>
2020-05-08 17:32:28 +0900

    fix: functions region to tokyo

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,65 +5,68 @@ admin.initializeApp(functions.config().firebase)
 admin.firestore().settings({ timestampsInSnapshots: true })
 
 /* eslint-disable import/prefer-default-export */
-export const setWord = functions.storage.object().onFinalize(o => {
-  if (!o.name) {
-    console.error('File not found')
-    return null
-  }
+export const setWord = functions
+  .region('asia-northeast1')
+  .storage.object()
+  .onFinalize(o => {
+    if (!o.name) {
+      console.error('File not found')
+      return null
+    }
 
-  const file = admin
-    .storage()
-    .bucket()
-    .file(o.name)
+    const file = admin
+      .storage()
+      .bucket()
+      .file(o.name)
 
-  if (!file.name) {
-    console.error('File.name not found')
-    return null
-  }
+    if (!file.name) {
+      console.error('File.name not found')
+      return null
+    }
 
-  const fileName = file.name
-    .split('/')
-    .slice(-1)[0]
-    .split('.')
-    .slice(0, -1) // Have names that include periods (NODE.JS, VUE.JS, etc..)
-    .join('.')
+    const fileName = file.name
+      .split('/')
+      .slice(-1)[0]
+      .split('.')
+      .slice(0, -1) // Have names that include periods (NODE.JS, VUE.JS, etc..)
+      .join('.')
 
-  file
-    .download()
-    .then(res => {
-      const category: string = (res[0]
-        .toString()
-        .match(/## category\n\n(.+)/) || ['', ''])[1].trim()
-      const titles: string[] = (res[0]
-        .toString()
-        .match(/## titles\n\n((.+\n)+)/) || ['', ''])[1]
-        .trim()
-        .split(/\n/)
-      const description: string = (res[0]
-        .toString()
-        .match(/## description\n\n((.+\n|\n)+)/) || ['', ''])[1].trim()
-      const descriptionByLine: string[] = (res[0]
-        .toString()
-        .match(/## description\n\n((.+\n|\n)+)/) || ['', ''])[1]
-        .trim()
-        .split(/\n/)
+    file
+      .download()
+      .then(res => {
+        const category: string = (res[0]
+          .toString()
+          .match(/## category\n\n(.+)/) || ['', ''])[1].trim()
+        const titles: string[] = (res[0]
+          .toString()
+          .match(/## titles\n\n((.+\n)+)/) || ['', ''])[1]
+          .trim()
+          .split(/\n/)
+        const description: string = (res[0]
+          .toString()
+          .match(/## description\n\n((.+\n|\n)+)/) || ['', ''])[1].trim()
+        const descriptionByLine: string[] = (res[0]
+          .toString()
+          .match(/## description\n\n((.+\n|\n)+)/) || ['', ''])[1]
+          .trim()
+          .split(/\n/)
 
-      admin
-        .firestore()
-        .collection('words')
-        .doc(fileName)
-        .set({ category, titles, description, descriptionByLine })
-        .catch((err: Error) =>
-          console.error(
-            `Document set error fileName=${fileName} err=${err.message}`
+        admin
+          .firestore()
+          .collection('words')
+          .doc(fileName)
+          .set({ category, titles, description, descriptionByLine })
+          .catch((err: Error) =>
+            console.error(
+              `Document set error fileName=${fileName} err=${err.message}`
+            )
           )
+      })
+      .catch((err: Error) =>
+        console.error(
+          `File download error fileName=${fileName} err=${err.message}`
         )
-    })
-    .catch((err: Error) =>
-      console.error(
-        `File download error fileName=${fileName} err=${err.message}`
       )
-    )
-  return null
-})
+    return null
+  })
 /* eslint-enable import/prefer-default-export */
