087433cbe10f04be587eb1ac45478dbd4cc16963
kurosame <kurosame1227@gmail.com>
2020-05-15 19:19:01 +0900

    fix: retry added to functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,65 +5,98 @@ admin.initializeApp(functions.config().firebase)
 admin.firestore().settings({ timestampsInSnapshots: true })
 
 /* eslint-disable import/prefer-default-export */
-export const setWord = functions.storage.object().onFinalize(o => {
-  if (!o.name) {
-    console.error('File not found')
-    return null
-  }
+export const setWord = functions
+  .runWith({ timeoutSeconds: 540 })
+  .storage.object()
+  .onFinalize(async o => {
+    if (!o.name) {
+      console.error('File not found')
+      return null
+    }
 
-  const file = admin
-    .storage()
-    .bucket()
-    .file(o.name)
+    const file = admin
+      .storage()
+      .bucket()
+      .file(o.name)
 
-  if (!file.name) {
-    console.error('File.name not found')
-    return null
-  }
+    if (!file.name) {
+      console.error('File.name not found')
+      return null
+    }
 
-  const fileName = file.name
-    .split('/')
-    .slice(-1)[0]
-    .split('.')
-    .slice(0, -1) // Have names that include periods (NODE.JS, VUE.JS, etc..)
-    .join('.')
+    const fileName = file.name
+      .split('/')
+      .slice(-1)[0]
+      .split('.')
+      .slice(0, -1) // Have names that include periods (NODE.JS, VUE.JS, etc..)
+      .join('.')
 
-  file
-    .download()
-    .then(res => {
-      const category: string = (res[0]
-        .toString()
-        .match(/## category\n\n(.+)/) || ['', ''])[1].trim()
-      const titles: string[] = (res[0]
-        .toString()
-        .match(/## titles\n\n((.+\n)+)/) || ['', ''])[1]
-        .trim()
-        .split(/\n/)
-      const description: string = (res[0]
-        .toString()
-        .match(/## description\n\n((.+\n|\n)+)/) || ['', ''])[1].trim()
-      const descriptionByLine: string[] = (res[0]
-        .toString()
-        .match(/## description\n\n((.+\n|\n)+)/) || ['', ''])[1]
-        .trim()
-        .split(/\n/)
+    await file
+      .download()
+      .then(async res => {
+        const category: string = (res[0]
+          .toString()
+          .match(/## category\n\n(.+)/) || ['', ''])[1].trim()
+        const titles: string[] = (res[0]
+          .toString()
+          .match(/## titles\n\n((.+\n)+)/) || ['', ''])[1]
+          .trim()
+          .split(/\n/)
+        const description: string = (res[0]
+          .toString()
+          .match(/## description\n\n((.+\n|\n)+)/) || ['', ''])[1].trim()
+        const descriptionByLine: string[] = (res[0]
+          .toString()
+          .match(/## description\n\n((.+\n|\n)+)/) || ['', ''])[1]
+          .trim()
+          .split(/\n/)
 
-      admin
-        .firestore()
-        .collection('words')
-        .doc(fileName)
-        .set({ category, titles, description, descriptionByLine })
-        .catch((err: Error) =>
-          console.error(
-            `Document set error fileName=${fileName} err=${err.message}`
+        const doSet = async (): Promise<void> => {
+          admin
+            .firestore()
+            .collection('words')
+            .doc(fileName)
+            .set({ category, titles, description, descriptionByLine })
+            .catch((err: Error) =>
+              console.error(
+                `Document set error fileName=${fileName} err=${err.message}`
+              )
+            )
+          console.info(`Run document set fileName=${fileName}`)
+          await new Promise(resolve => setTimeout(resolve, 120000)) // Sleep for 2 minutes
+          return admin
+            .firestore()
+            .collection('words')
+            .doc(fileName)
+            .get()
+            .then(doc => {
+              if (!doc.exists) return Promise.reject()
+              const data = doc.data() || {
+                category: '',
+                titles: [],
+                description: ''
+              }
+              return category === data.category &&
+                JSON.stringify(titles) === JSON.stringify(data.titles) &&
+                description === data.description
+                ? Promise.resolve()
+                : Promise.reject()
+            })
+            .catch(() => Promise.reject())
+        }
+
+        await doSet()
+          .catch(doSet)
+          .catch(doSet)
+          .catch(() =>
+            console.error(`Document not updates fileName=${fileName}`)
           )
+      })
+      .catch((err: Error) =>
+        console.error(
+          `File download error fileName=${fileName} err=${err.message}`
         )
-    })
-    .catch((err: Error) =>
-      console.error(
-        `File download error fileName=${fileName} err=${err.message}`
       )
-    )
-  return null
-})
+    return null
+  })
 /* eslint-enable import/prefer-default-export */
