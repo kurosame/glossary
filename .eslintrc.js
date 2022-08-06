a78913e61f315b72cd61f63b953a4f963f7cc4e7
kurosame <kurosame1227@gmail.com>
2022-08-06 17:27:40 +0900

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
         ],
         pathGroupsExcludedImportTypes: ['builtin'],
         'newlines-between': 'always',
-        alphabetize: { order: 'asc' }
+        alphabetize: { order: 'asc', caseInsensitive: true }
       }
     ],
     'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
